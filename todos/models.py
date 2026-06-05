69593044cd523a32e282d327bac5c62eaa95ae70
jesche487 <jgchen@usc.edu>
2026-06-05 11:31:47 -0700

    feat: add Todo model and initial migration

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Database table
+class Todo(models.Model):
+    title = models.CharField(max_length=200)                # VARCHAR
+    description = models.TextField(blank=True)              # TEXT
+    completed = models.BooleanField(default=False)          # BOOLEAN
+    created_at = models.DateTimeField(auto_now_add=True)    # set on INSERT, not touched again
+    updated_at = models.DateTimeField(auto_now=True)        # updated one every save automatically
+
+    def __str__(self):
+        return self.title
