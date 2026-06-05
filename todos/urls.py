47e1634819ff60f9de81ff68d47a83492d901fe6
jesche487 <jgchen@usc.edu>
2026-06-05 13:20:10 -0700

    feat: Added webpage UI to existing functionality, made a checklist app. Fixed long text overflow and API error feedback bugs

--- todos/urls.py
+++ todos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index),  
     path('todos/', views.todo_list),
     path('todos/<int:pk>/', views.todo_detail),
 ]
--- todos/views.py
+++ todos/views.py
@@ -3,7 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Todo
 from .serializers import TodoSerializer
+from django.shortcuts import render
 
+def index(request):
+    return render(request, 'todos/index.html')
 
 @api_view(['GET', 'POST'])
 def todo_list(request):
